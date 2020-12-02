fdd1fe7791f41eb248e118b25313cecf39bca158
rixong <rixong@gmail.com>
2020-12-02 12:58:56 -0600

    setting up socket.io

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const path = require('path')
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 console.log(publicDirectoryPath)
 // app.use(esxpress.JSON());
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('New web socket connection');
+})
+
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
