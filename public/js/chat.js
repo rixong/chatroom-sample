aed54cc689a053c6b07f4b0ac76008dc75fbc769
rixong <rixong@gmail.com>
2020-12-03 07:34:48 -0600

    basic socket.io function

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,46 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+  // console.log(message);
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit('sendMessage', message, (error) => {
+    if(error){
+      return console.log(error)
+    }
+    console.log(message);
+  });
+
+  e.target.message.value = '';
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported');
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position)
+    socket.emit('sendLocation',
+      {lat: position.coords.latitude,
+        long: position.coords.longitude
+      })
+  });
+});
+
+
+
+
+// const button = document.querySelector('#increment').addEventListener('click', () => {
+//   // console.log('Clicked');
+//   socket.emit('increment')
+// })
+
+// const display = document.querySelector('#display');
+// socket.on('countUpdated', (count) => {
+//   // console.log("The count is updated.", count);
+//   display.textContent = count;
+// })
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-
+const Filter = require('bad-words');
 
 
 const app = express();
@@ -15,8 +15,36 @@ console.log(publicDirectoryPath)
 // app.use(esxpress.JSON());
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+
+io.on('connection', (socket) => {
   console.log('New web socket connection');
+
+  socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new member has joined.')
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+    if(filter.isProfane(message)){
+      return callback('Profanity is not allowed!')
+    }
+    io.emit('message',  message)
+    callback();
+  })
+  
+    socket.on('sendLocation', (coords) => {
+      io.emit('message', `https://google.com/maps?q=${coords.lat},${coords.long}`)
+    })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A person has left the chat.')
+  })
+
+  // Count challenge code
+  // socket.emit('countUpdated', count)
+  // socket.on('increment', () => {
+  //   count ++;
+  //   io.emit('countUpdated', count)
+  // })
 })
 
 server.listen(port, () => {
